72967b2866d516dd71b6f6c00911352ce97c2188
devlos0322 <devlos0322@gmail.com>
2021-10-28 18:03:39 +0900

    Initial commit

---
+++ src/main/java/org/devlos/practicebootappuser/PracticeBootappUserApplication.java
@@ -0,0 +1,13 @@
+package org.devlos.practicebootappuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBootappUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBootappUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devlos/practicebootappuser/PracticeBootappUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devlos.practicebootappuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBootappUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
